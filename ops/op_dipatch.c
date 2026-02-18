682eb67dba449469b1cfac2df2930c3db125f5ea
haneen tawalbeh <haneentawalbeh32@gmail.com>
2026-02-18 22:19:13 +0300

    done with ops

--- ops/op_dipatch.c
+++ ops/op_dipatch.c
@@ -11,3 +11,31 @@
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	op_dispatch(char *op, t_stack *a, t_stack *b)
+{
+	if (ft_strncmp(op, "pa", 3) == 0)
+		pa(a, b);
+	else if (ft_strncmp(op, "pb", 3) == 0)
+		pb(b, a);
+	else if (ft_strncmp(op, "sa", 3) == 0)
+		swap_printer(a, 'a');
+	else if (ft_strncmp(op, "sb", 3) == 0)
+		swap_printer(b, 'b');
+	else if (ft_strncmp(op, "ss", 3) == 0)
+		ss(a, b);
+	else if (ft_strncmp(op, "ra", 3) == 0)
+		rotate_printer(a, 'a');
+	else if (ft_strncmp(op, "rb", 3) == 0)
+		rotate_printer(b, 'b');
+	else if (ft_strncmp(op, "rr", 3) == 0)
+		rr(a, b);
+	else if (ft_strncmp(op, "rra", 4) == 0)
+		rev_rotate_printer(a, 'a');
+	else if (ft_strncmp(op, "rrb", 4) == 0)
+		rev_rotate_printer(b, 'b');
+	else if (ft_strncmp(op, "rrr", 4) == 0)
+		rrr(a, b);
+	else
+		write(2, "Error\n", 6);
+}
